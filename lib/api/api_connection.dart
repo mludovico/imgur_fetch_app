bd05835f8045b6ed435e3265a1dc7df15f4412b0
Marcelo Ludovico <maludovico@msn.com>
2020-06-21 14:22:48 -0300

    Added filter to search query to get only jpeg images Added filters to fetched items to get only jpeg image and to avoid invalid links

--- lib/api/api_connection.dart
+++ lib/api/api_connection.dart
@@ -7,7 +7,7 @@ class ApiConnection{
 
   Future<List> getImages(int offset) async {
     String _query = 'cats';
-    String getUrl = '$basePath/gallery/search?q=$_query';
+    String getUrl = '$basePath/gallery/search?q=$_query&q_type=jpeg';
     print(getUrl);
     http.Response response = await http.get(
       getUrl,
@@ -18,11 +18,20 @@ class ApiConnection{
     print(response.statusCode);
     List lista = [];
     if(response.statusCode == 200) {
-      for (int i = offset * 1; i <= offset * 6; i++) {
-        lista.add(json.decode(response.body)['data'][i]['images'][0]['link']);
+      var responseObject = json.decode(response.body);
+      //print(responseObject['data']);
+      var items = responseObject['data'].map((item){
+        return item['images'].where((image)=>image['type'] == 'image/jpeg').toList();
+      });
+      items.where((item)=>item != null).where((item)=>!item.isEmpty).forEach((item){
+        print(item[0]['type']);
+        print(item[0]['link']);
       }
-      print(lista);
-      return lista;
+      );
+      var links = items.where((item)=>item != null).where((item)=>!item.isEmpty)
+        .map((item)=>item[0]['link']).toList();
+      print(links);
+      return links;
     }
     else
       return null;
